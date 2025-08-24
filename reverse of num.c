78538b265d3044ff000f2b737ec0b9d665a48aa8
MUNTAHA <muntahamoujim@gmail.com>
2025-08-25 01:46:41 +0600

    Add files via upload

---
+++ reverse of num.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+    int x,i,sum=0;
+    printf("Enter value: ");
+    scanf("%d",&x);
+    while(x!=0)
+    {
+        i=x%10;
+        sum=sum*10+i;
+        x=x/10;
+    }
+    printf("sum=%d",sum);
+    return 0;
+}
